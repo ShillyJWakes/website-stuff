f4b8493b7964d8a84ac576caf60a12a7a630162c
Dennis Donathan II <89102530+ShillyJWakes@users.noreply.github.com>
2023-04-06 13:33:50 -0500

    Modify term table

--- frontend/src/app/shared/components/term-table/term-table.component.ts
+++ frontend/src/app/shared/components/term-table/term-table.component.ts
@@ -49,7 +49,7 @@ export class TermTableComponent implements OnInit, OnDestroy {
   ngOnInit(): void {
     this.actions = {
       edit: true,
-      delete: false,
+      delete: true,
       add: true,
       custom: [
         {
