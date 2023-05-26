e9edd437a962ecb8dc508fea18956b91cafc0d2a
Dennis Donathan II <dmiles.donathan@gmail.com>
2023-05-26 13:23:32 -0500

    gitignore fixes

--- backend/gunicorn.conf.py
+++
@@ -1,8 +0,0 @@
-import multiprocessing
-
-errorlog = '/var/www/website-stuff/backend/gunicorn_error.log'
-loglevel = 'info'
-bind = '192.168.1.203:5000'
-workers = multiprocessing.cpu_count() * 2 + 1
-user = 'sis-portal'
-group = 'www-data'
